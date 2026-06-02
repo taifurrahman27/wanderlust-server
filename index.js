0f333f7120f7661bc14dd92663e5320188873010
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-06-03 00:41:34 +0600

    Data read by get method

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dontenv.config();
 
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+
+async function run() {
+    try {
+
+        const db = client.db("wanderlust");
+        const destinationCollection = db.collection("destinations");
+
+        app.get("/destination", async (req, res) => {
+            const result = await destinationCollection.find().toArray();
+            res.json(result);
+        });
+
+        app.post("/destination", async (req, res) => {
+            const destinationData = req.body;
+            console.log(destinationData);
+            const result = await destinationCollection.insertOne(destinationData);
+
+            res.json(result);
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!",
+        );
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Server is running on this page");
+    res.send("Server is running on the local server!");
 });
 
 app.listen(PORT, () => {
